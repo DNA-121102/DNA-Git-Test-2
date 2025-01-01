5c5afaff6b2f6067500163ca96db8f37c0b84cfa
DNA-121102 <danimille88@gmail.com>
2025-01-02 03:03:52 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
         <h1>Hello World!</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt tempora praesentium quia possimus asperiores, facere ea quisquam consequuntur optio incidunt, iste autem vel, enim recusandae debitis minus illum maxime atque?</p>
+        <ul>
+            <li>satu</li>
+            <li>dua</li>
+            <li>tiga</li>
+        </ul>
 </body>
 </html>
